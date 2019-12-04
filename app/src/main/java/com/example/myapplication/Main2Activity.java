9453544ea4423980bf0764518d2472c8462ce857
Victor Janampa <torrviccc@gmail.com>
2019-12-04 13:18:45 -0500

    Photo buttons added

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,21 +1,63 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
 
+    final int COD_SELECCIONA=10;
+    final int COD_FOTO=20;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        imageView=findViewById(R.id.imageView);
     }
 
     public void register(View view) {
         Intent i = new Intent(this, Main3Activity.class);
         startActivity(i);
     }
+
+    public void cargarImagen(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione la aplicacion"),COD_SELECCIONA);
+    }
+
+    public void takePicture(View view){
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,COD_FOTO);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            switch (requestCode) {
+                case COD_SELECCIONA:
+                    Uri miPath = data.getData();
+                    imageView.setImageURI(miPath);
+                    break;
+
+                case COD_FOTO:
+                    //Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
+                    Bundle ext = data.getExtras();
+                    Bitmap bitmap = (Bitmap)ext.get("data");
+                    imageView.setImageBitmap(bitmap);
+                    break;
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/Main3Activity.java
+++ app/src/main/java/com/example/myapplication/Main3Activity.java
@@ -4,8 +4,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,7 +19,9 @@ import com.google.android.material.textfield.TextInputLayout;
 import org.w3c.dom.Text;
 
 public class Main3Activity extends AppCompatActivity {
-
+    final int COD_SELECCIONA=10;
+    final int COD_FOTO=20;
+    ImageView imageView;
     TextInputLayout tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +35,41 @@ public class Main3Activity extends AppCompatActivity {
         startActivityForResult(i,15);
     }
 
+    public void cargarImagen(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione la aplicacion"),COD_SELECCIONA);
+    }
+
+    public void takePicture(View view){
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,COD_FOTO);
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 15 && resultCode == RESULT_OK){
-            String barcode = data.getStringExtra("value");
-            Toast.makeText(this, barcode, Toast.LENGTH_SHORT).show();
-            tv1.getEditText().setText(barcode);
+        if (resultCode == RESULT_OK) {
+
+            switch (requestCode) {
+                case COD_SELECCIONA:
+                    Uri miPath = data.getData();
+                    imageView.setImageURI(miPath);
+                    break;
+
+                case COD_FOTO:
+                    //Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
+                    Bundle ext = data.getExtras();
+                    Bitmap bitmap = (Bitmap)ext.get("data");
+                    imageView.setImageBitmap(bitmap);
+                    break;
+
+                case 15:
+                    String barcode = data.getStringExtra("value");
+                    Toast.makeText(this, barcode, Toast.LENGTH_SHORT).show();
+                    tv1.getEditText().setText(barcode);
+                    break;
+            }
         }
     }
+
 }
